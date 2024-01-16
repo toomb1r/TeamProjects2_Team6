927d6de4384c2af6a4f76d270afcfbadf8b2f213
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2024-01-16 13:51:46 -0500

    Cleaned up and documented PEAT's encryption and decryption code. The key used in encryption needs to be changed to the controller's public key when that code is written

--- PEAT/main.py
+++ PEAT/main.py
@@ -1,34 +1,47 @@
-# from gps3 import gps3
-# import RPi.GPIO
+from gps3 import gps3
+import RPi.GPIO
 import random
-# import digitalio
-# import board
-# import busio
-# import adafruit_rfm9x
+import digitalio
+import board
+import busio
+import adafruit_rfm9x
 import rsa # pip install rsa
 
 def encrypt(msg):
-    # with open("/Users/anmolsaini/.ssh/peat.pub","r") as key_pub_file:
-    #     key_pub = key_pub_file.read()
-    #     encoded_msg = msg.encode('utf8')
-    #     encrypted_msg = rsa.encrypt(encoded_msg, key_pub)
-    #     print(encrypted_msg)
+    """Encrypts a message using the controller's public key
+    Gets the RSA public key of the controller. Encodes the message in UTF-8 and encrypts it.
+    Returns the encrypted message.
 
-    fKey = open('/Users/anmolsaini/.ssh/peat.pub.pem','rb')
-    #publicKey = rsa.PublicKey.load_pkcs1_openssl_pem(fKey.read())
-    publicKey = rsa.PublicKey.load_pkcs1(fKey.read())
+    Args:
+        msg (str): The message to be encrypted
+
+    Returns:
+        bytes: The encrypted message
+    """
+
+    fkey = open('/Users/anmolsaini/.ssh/peat.pub.pem','rb')
+    public_key = rsa.PublicKey.load_pkcs1(fkey.read())
     encoded_msg = msg.encode('utf8')
-    encrypted_msg = rsa.encrypt(encoded_msg, publicKey)
-    print(encrypted_msg)
+    encrypted_msg = rsa.encrypt(encoded_msg, public_key)
     return encrypted_msg
 
 def decrypt(encrypted_msg):
-    fKey = open('/Users/anmolsaini/.ssh/peat', 'rb')  # Assuming private key file is 'peat'
-    privateKey = rsa.PrivateKey.load_pkcs1(fKey.read())
-    decrypted_msg = rsa.decrypt(encrypted_msg, privateKey).decode('utf8')
-    print(decrypted_msg)
-    return decrypted_msg
+    """Decrypts a message using PEAT's private key
+    Gets the RSA public key of the controller. Decrypts the message and decodes it from UTF-8.
+    Returns the decrypted message.
 
-#print("hello world")
-enc_msg = encrypt("hello")
+    Args:
+        encrypted_msg (bytes): The message to be decrypted
+
+    Returns:
+        str: The decrypted message
+    """
+
+    fkey = open('/Users/anmolsaini/.ssh/peat', 'rb')
+    private_key = rsa.PrivateKey.load_pkcs1(fkey.read())
+    decrypted_msg = rsa.decrypt(encrypted_msg, private_key)
+    decoded_msg = decrypted_msg.decode('utf8')
+    return decoded_msg
+
+enc_msg = encrypt("this is encrypted")
 dec_msg = decrypt(enc_msg)
