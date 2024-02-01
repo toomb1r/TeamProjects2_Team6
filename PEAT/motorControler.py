3c9a7a08c523d284c878156e9864d3e547467e69
Matthew0809 <sanders.247@wright.edu>
2024-02-01 09:06:47 -0500

    motor control

---
+++ PEAT/motorControler.py
@@ -0,0 +1,117 @@
+import RPi.GPIO as GPIO          
+from time import sleep
+
+in1 = 24
+in2 = 23
+en = 25
+temp1=1
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(en,GPIO.OUT)
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
+p=GPIO.PWM(en,1000)
+p.start(25)
+print("\n")
+print("The default speed & direction of motor is LOW & Forward.....")
+print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
+print("\n")    
+
+while(1):
+
+    x=raw_input()
+    
+    if x=='r':
+        print("run")
+        if(temp1==1):
+         GPIO.output(in1,GPIO.HIGH)
+         GPIO.output(in2,GPIO.LOW)
+         print("forward")
+         x='z'
+        else:
+         GPIO.output(in1,GPIO.LOW)
+         GPIO.output(in2,GPIO.HIGH)
+         print("backward")
+         x='z'
+
+
+    elif x=='s':
+        print("stop")
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.LOW)
+        x='z'
+
+    elif x=='f':
+        print("forward")
+        GPIO.output(in1,GPIO.HIGH)
+        GPIO.output(in2,GPIO.LOW)
+        temp1=1
+        x='z'
+
+    elif x=='b':
+        print("backward")
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.HIGH)
+        temp1=0
+        x='z'
+
+    elif x=='set1':
+        print("low1")
+        p.ChangeDutyCycle(10)
+        x='z'
+
+    elif x=='set2':
+        print("low2")
+        p.ChangeDutyCycle(20)
+        x='z'
+
+    elif x=='set3':
+        print("low1")
+        p.ChangeDutyCycle(30)
+        x='z'
+
+    elif x=='set4':
+        print("medium1")
+        p.ChangeDutyCycle(40)
+        x='z'
+
+    elif x=='set5':
+        print("medium2")
+        p.ChangeDutyCycle(50)
+        x='z'
+
+    elif x=='set6':
+        print("medium3")
+        p.ChangeDutyCycle(60)
+        x='z'
+
+    elif x=='set7':
+        print("high1")
+        p.ChangeDutyCycle(70)
+        x='z'
+
+    elif x=='set8':
+        print("high2")
+        p.ChangeDutyCycle(80)
+        x='z'
+
+    elif x=='set9':
+        print("high3")
+        p.ChangeDutyCycle(90)
+        x='z'
+
+    elif x=='set10':
+        print("MAXIMUM OVERDRIVE")
+        p.ChangeDutyCycle(100)
+        x='z'
+     
+    
+    elif x=='e':
+        GPIO.cleanup()
+        break
+    
+    else:
+        print("<<<  wrong data  >>>")
+        print("please enter the defined data to continue.....")
