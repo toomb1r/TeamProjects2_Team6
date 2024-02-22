ebea0d2f717fe199f57f1cbbcea9f654207e75a4
Tiffany Behr <97482507+toomb1r@users.noreply.github.com>
2024-02-22 09:38:04 -0500

    back to bcm wth

--- PEAT/utils/light.py
+++ PEAT/utils/light.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 #https://robocraze.com/blogs/post/turning-on-an-led-with-your-raspberry-pi-s-gpio-pins 
 # Set the GPIO mode
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 # GPIO pin number
 gpio_pin = 13
