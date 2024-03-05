786e8f640033d774e8c303942b4201ec4697fd43
Tiffany Behr <97482507+toomb1r@users.noreply.github.com>
2024-03-05 09:06:33 -0500

    docstrings

--- PEAT/utils/light.py
+++ PEAT/utils/light.py
@@ -9,9 +9,15 @@ gpio_pin = 13
 GPIO.setup(gpio_pin, GPIO.OUT)
 
 def turnOnLight():
-    # Setup the GPIO pin as output
+     """Turns on the light of PEAT 
+    Energizes Pin 13 which is currently initialized within this file
 
+    Args:
+        None
 
+    Returns:
+        None
+    """
     # Turn on the light
     GPIO.output(gpio_pin, GPIO.HIGH)
     print("Light is ON")
@@ -22,5 +28,13 @@ def turnOnLight():
 
 
 def turnOffLight():
+        """Turns off the light of peat. pin 13 as defined within file
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
     GPIO.output(gpio_pin, GPIO.LOW)
     print("Light is OFF")
