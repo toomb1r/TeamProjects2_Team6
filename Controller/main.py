c7bde15d9792868488ae67d280d0e55254eae55b
Regin Potter <potter.70@wright.edu>
2024-03-07 08:48:23 -0500

    Added testing for emergency stop

--- Controller/main.py
+++ Controller/main.py
@@ -3,12 +3,14 @@ import board
 import busio
 import digitalio
 import RPi.GPIO
+from time import sleep
 
 from utils.communications import *
 from utils.actions import *
 
-GPIO.setmode(GPIO.BOARD)
-
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(20,GPIO.OUTPUT)
+GPIO.add_event_detect(20, GPIO.FALLING, callback=emergency_stop(), bouncetime=100)
 def main():
     """Executes the main functionality of the Controller
 
@@ -16,10 +18,13 @@ def main():
 
     Returns: None
     """
-
-    enc_msg = encrypt("this is encrypted")
-    dec_msg = decrypt(enc_msg)
-    GPIO.add_event_detect(17, GPIO.FALLING, callback=emergencyStop(), bouncetime=100)
+    while True:
+        GPIO.output(20, GPIO.HIGH)
+        sleep(1)
+        GPIO.output(20, GPIO.LOW)
+    # enc_msg = encrypt("this is encrypted")
+    # dec_msg = decrypt(enc_msg)
+    # GPIO.add_event_detect(20, GPIO.FALLING, callback=emergencyStop(), bouncetime=100)
 
 if __name__ == "__main__":
     main()
--- Controller/utils/actions.py
+++ Controller/utils/actions.py
@@ -1,24 +1,42 @@
-import RPi.GPIO
+import RPi.GPIO as GPIO
 from utils.communications import *
+from time import sleep
 
-GPIO.setup(17, INPUT)
+GPIO.setmode(GPIO.BCM)
+# GPIO.setup(20, GPIO.OUTPUT)
+GPIO.setup(21, GPIO.OUTPUT)
+
+stop = False
 
 def emergency_stop():
-    """
-    Sends a signal to PEAT to stop all functions
-    Reads in the signal from the emergency stop button
-    If the button is pressed it will encrypt and send the message to PEAT
+    global stop
 
-    Args:
-        None
+    GPIO.output(20, GPIO.HIGH)
+    GPIO.output(21, GPIO.HIGH)
+    stop = not stop
+    if stop:
+        GPIO.output(20, GPIO.HIGH)
+        GPIO.output(21, GPIO.HIGH)
+        while True:
+            sleep(1)
+    else:
+        GPIO.output(20, GPIO.LOW)
+        GPIO.output(21, GPIO.LOW)
+    # """
+    # Sends a signal to PEAT to stop all functions
+    # Reads in the signal from the emergency stop button
+    # If the button is pressed it will encrypt and send the message to PEAT
 
-    Returns:
-        None
-    """
+    # Args:
+    #     None
 
-    # If the button has been pressed
-    # Encrypt the word "stop" and send it
-    if(GPIO.input(17)):
-        signal = encrypt("stop")
-        # Send signal
-        return signal
+    # Returns:
+    #     None
+    # """
+
+    # # If the button has been pressed
+    # # Encrypt the word "stop" and send it
+    # if(GPIO.input(17)):
+    #     signal = encrypt("stop")
+    #     # Send signal
+    #     return signal
