9786e465be817b366f0cbccd1bbd04b1594ae607
Regin Potter <potter.70@wright.edu>
2024-03-08 14:30:22 -0500

    Added ultrasonic sensor preliminary code Added method for ultrasonic sensor detection Added method to use ultrasonic sensor detection to send the user an alert

--- PEAT/utils/algaecide.py
+++ PEAT/utils/algaecide.py
@@ -21,11 +21,42 @@ dispenserpwm.start(25)
 augerpwm.ChangeDutyCycle(75)
 dispenserpwm.ChangeDutyCycle(75)
 
+def ultson_algae():
+    """
+    Measures algaecide left
+
+    Uses an ultrasonic sensor to measure how much algaecide is left
+    Returns the distance from the ultrasonic sensor
+
+    Args:
+        None
+    Returns:
+        distance (int): distance from the ultrasonic sensor
+    """
+    distance = 0
+
+def detect_out():
+    """
+    Detects when PEAT is out of algaecide
+
+    Uses an ultrasonic sensor to detect whether it is > 6cm
+    If so it returns True
+
+    Args:
+        None
+    Returns:
+        out (boolean): Whether PEAT is out of algaecide
+    """
+    out = False
+    if ultson_algae > 6:
+        out = True
+    return out
+
 def dispense_algae():
     """
     Starts the dispensing of algaecide
-    
-    Turns on the auger and dispenser motor
+
+    Turns on the auger and dispenser motors
 
     Args:
         None
@@ -37,8 +68,21 @@ def dispense_algae():
     GPIO.output(dispenser1, GPIO.HIGH)
     GPIO.output(dispenser2, GPIO.LOW)
 
+def stop_dispense():
+    """
+    Stops the dispensing of algaecide
 
+    Turns off the auger and dispenser motors
 
+    Args:
+        None
+    Returns:
+        None
+    """
+    GPIO.output(auger1, GPIO.LOW)
+    GPIO.output(auger2, GPIO.LOW)
+    GPIO.output(dispenser1, GPIO.LOW)
+    GPIO.output(dispenser2, GPIO.LOW)
 
 def change_dispense_speed(speed):
     """
