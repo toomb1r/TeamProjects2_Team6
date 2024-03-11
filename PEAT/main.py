fcc4ff55fb43eef984b4066041eacadd4e99776d
ReginPotter <potter.70@wright.edu>
2024-03-11 18:44:29 -0400

    Testing stuff for algaecide

--- PEAT/main.py
+++ PEAT/main.py
@@ -5,7 +5,7 @@ import digitalio
 from gps3 import gps3
 
 from utils.communications import *
-from utils.movement import *
+#from utils.movement import *
 from utils.algaecide import *
 
 def main():
@@ -16,22 +16,30 @@ def main():
     Returns: None
     """
 
-    enc_msg = encrypt("this is encrypted")
-    dec_msg = decrypt(enc_msg)
+    #enc_msg = encrypt("this is encrypted")
+    #dec_msg = decrypt(enc_msg)
 
     # This is to start the servo motor in the center of the 180 degrees
     # To allow -90 and 90 degrees of motion
-    turning(90)
+    #turning(90)
 
-    move()
-    rorl = True
-    dispense_algae()
-    change_dispense_speed(100)
+    #move()
+    #rorl = True
+    #dispense_algae()
+    #dispense_algae()
+    #change_dispense_speed(100)
 
     while(True):
+        if detect_out():
+            print("out!\n")
+        else:
+            print("not out\n")
+        #print("enter new speed")
+        #speed = int(input())
+        #change_dispense_speed(speed)
         # detect_out()
-        edgeOfPond(rorl)
-        move()
+        #edgeOfPond(rorl)
+        #move()
 
 if __name__ == "__main__":
     main()
