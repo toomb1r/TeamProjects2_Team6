8698f108de7963c8c1de7545324b0da3b95f5338
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2024-03-11 18:55:49 -0400

    sending sigs from controller to peat

--- Controller/utils/communications.py
+++ Controller/utils/communications.py
@@ -1,13 +1,50 @@
-import rsa # pip install rsa
-
-import time
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+import adafruit_rfm9x
+import board
 import busio
 from digitalio import DigitalInOut
-import board
-import adafruit_rfm9x
+import rsa # pip install rsa
 import RPi.GPIO as GPIO
+from time import sleep
 
 GPIO.setmode(GPIO.BCM)
+
+IMMOBILIZED_LIGHT = 21 # signal 1/2
+OUT_OF_ALGAECIDE_LIGHT = 20 # signal 3/4
+GPIO.setup(IMMOBILIZED_LIGHT, GPIO.OUT)
+GPIO.setup(OUT_OF_ALGAECIDE_LIGHT, GPIO.OUT)
+
+SET_HOME_BUTTON = 18 # signal 5/6
+GPIO.setup(SET_HOME_BUTTON, GPIO.IN)
+GPIO.setup(SET_HOME_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+RETURN_TO_HOME_BUTTON = 14 # signal 7/8
+GPIO.setup(RETURN_TO_HOME_BUTTON, GPIO.IN)
+GPIO.setup(RETURN_TO_HOME_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+START_STOP_MOVE_BUTTON = 16 # signal 9/10
+GPIO.setup(START_STOP_MOVE_BUTTON, GPIO.IN)
+GPIO.setup(START_STOP_MOVE_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+START_STOP_DISPENSING_BUTTON = 24 # signal 11/12
+GPIO.setup(START_STOP_DISPENSING_BUTTON, GPIO.IN)
+GPIO.setup(START_STOP_DISPENSING_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+EMERGENCY_STOP_BUTTON = 15 # signal 13/14
+GPIO.setup(EMERGENCY_STOP_BUTTON, GPIO.IN)
+GPIO.setup(EMERGENCY_STOP_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+DISPENSE_RATE = 23
+GPIO.setup(DISPENSE_RATE, GPIO.IN)
+GPIO.setup(DISPENSE_RATE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Initialize the I2C interface
+i2c = busio.I2C(board.SCL, board.SDA)
+# Create an  ADS1115 object
+ads = ADS.ADS1115(i2c)
+# Define the analog input channel
+channel = AnalogIn(ads, ADS.P0)
 
 def encrypt(msg):
     """Encrypts a message using PEAT's public key
@@ -49,109 +86,109 @@ def decrypt(encrypted_msg):
     decoded_msg = decrypted_msg.decode('utf8')
     return decoded_msg
 
-def transmit_and_receive():
-    # SPDX-FileCopyrightText: 2018 Brent Rubell for Adafruit Industries
-    #
-    # SPDX-License-Identifier: MIT
+# def transmit_and_receive():
+#     # SPDX-FileCopyrightText: 2018 Brent Rubell for Adafruit Industries
+#     #
+#     # SPDX-License-Identifier: MIT
 
-    """
-    Example for using the RFM9x Radio with Raspberry Pi.
+#     """
+#     Example for using the RFM9x Radio with Raspberry Pi.
 
-    Learn Guide: https://learn.adafruit.com/lora-and-lorawan-for-raspberry-pi
-    Author: Brent Rubell for Adafruit Industries
-    """
-    # # Import Python System Libraries
-    # import time
-    # # Import Blinka Libraries
-    # import busio
-    # from digitalio import DigitalInOut
-    # import board
-    # # Import the SSD1306 module.
-    # # import adafruit_ssd1306
-    # # Import RFM9x
-    # import adafruit_rfm9x
+#     Learn Guide: https://learn.adafruit.com/lora-and-lorawan-for-raspberry-pi
+#     Author: Brent Rubell for Adafruit Industries
+#     """
+#     # # Import Python System Libraries
+#     # import time
+#     # # Import Blinka Libraries
+#     # import busio
+#     # from digitalio import DigitalInOut
+#     # import board
+#     # # Import the SSD1306 module.
+#     # # import adafruit_ssd1306
+#     # # Import RFM9x
+#     # import adafruit_rfm9x
 
-    # # Button A
-    # btnA = DigitalInOut(board.D5)
-    # btnA.direction = Direction.INPUT
-    # btnA.pull = Pull.UP
+#     # # Button A
+#     # btnA = DigitalInOut(board.D5)
+#     # btnA.direction = Direction.INPUT
+#     # btnA.pull = Pull.UP
 
-    # # Button B
-    # btnB = DigitalInOut(board.D6)
-    # btnB.direction = Direction.INPUT
-    # btnB.pull = Pull.UP
+#     # # Button B
+#     # btnB = DigitalInOut(board.D6)
+#     # btnB.direction = Direction.INPUT
+#     # btnB.pull = Pull.UP
 
-    # # Button C
-    # btnC = DigitalInOut(board.D12)
-    # btnC.direction = Direction.INPUT
-    # btnC.pull = Pull.UP
+#     # # Button C
+#     # btnC = DigitalInOut(board.D12)
+#     # btnC.direction = Direction.INPUT
+#     # btnC.pull = Pull.UP
 
-    # Create the I2C interface.
-    # i2c = busio.I2C(board.SCL, board.SDA)
+#     # Create the I2C interface.
+#     # i2c = busio.I2C(board.SCL, board.SDA)
 
-    # # 128x32 OLED Display
-    # reset_pin = DigitalInOut(board.D4)
-    # display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
-    # # Clear the display.
-    # display.fill(0)
-    # display.show()
-    # width = display.width
-    # height = display.height
+#     # # 128x32 OLED Display
+#     # reset_pin = DigitalInOut(board.D4)
+#     # display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
+#     # # Clear the display.
+#     # display.fill(0)
+#     # display.show()
+#     # width = display.width
+#     # height = display.height
 
-    # Configure LoRa Radio
-    CS = DigitalInOut(board.CE1)
-    RESET = DigitalInOut(board.D25)
-    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-    rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
-    rfm9x.tx_power = 23
-    prev_packet = None
+#     # Configure LoRa Radio
+#     CS = DigitalInOut(board.CE1)
+#     RESET = DigitalInOut(board.D25)
+#     spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+#     rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+#     rfm9x.tx_power = 23
+#     prev_packet = None
 
-    while True:
-        packet = None
-        # draw a box to clear the image
-        # display.fill(0)
-        # display.text('RasPi LoRa', 35, 0, 1)
+#     while True:
+#         packet = None
+#         # draw a box to clear the image
+#         # display.fill(0)
+#         # display.text('RasPi LoRa', 35, 0, 1)
 
-        # check for packet rx
-        packet = rfm9x.receive()
-        if packet is None:
-            # display.show()
-            # display.text('- Waiting for PKT -', 15, 20, 1)
-            print("packet = None")
-        else:
-            # Display the packet text and rssi
-            # display.fill(0)
-            prev_packet = packet
-            packet_text = str(prev_packet, "utf-8")
-            # display.text('RX: ', 0, 0, 1)
-            # display.text(packet_text, 25, 0, 1)
-            print(f"packet = {packet_text}")
-            time.sleep(1)
+#         # check for packet rx
+#         packet = rfm9x.receive()
+#         if packet is None:
+#             # display.show()
+#             # display.text('- Waiting for PKT -', 15, 20, 1)
+#             print("packet = None")
+#         else:
+#             # Display the packet text and rssi
+#             # display.fill(0)
+#             prev_packet = packet
+#             packet_text = str(prev_packet, "utf-8")
+#             # display.text('RX: ', 0, 0, 1)
+#             # display.text(packet_text, 25, 0, 1)
+#             print(f"packet = {packet_text}")
+#             sleep(1)
 
-        # if not btnA.value:
-        #     # Send Button A
-        #     display.fill(0)
-        data = bytes("This is data!\r\n","utf-8")
-        rfm9x.send(data)
-        #     display.text('Sent Button A!', 25, 15, 1)
-        # elif not btnB.value:
-        #     # Send Button B
-        #     display.fill(0)
-        #     button_b_data = bytes("Button B!\r\n","utf-8")
-        #     rfm9x.send(button_b_data)
-        #     display.text('Sent Button B!', 25, 15, 1)
-        # elif not btnC.value:
-        #     # Send Button C
-        #     display.fill(0)
-        #     button_c_data = bytes("Button C!\r\n","utf-8")
-        #     rfm9x.send(button_c_data)
-        #     display.text('Sent Button C!', 25, 15, 1)
+#         # if not btnA.value:
+#         #     # Send Button A
+#         #     display.fill(0)
+#         data = bytes("This is data!\r\n","utf-8")
+#         rfm9x.send(data)
+#         #     display.text('Sent Button A!', 25, 15, 1)
+#         # elif not btnB.value:
+#         #     # Send Button B
+#         #     display.fill(0)
+#         #     button_b_data = bytes("Button B!\r\n","utf-8")
+#         #     rfm9x.send(button_b_data)
+#         #     display.text('Sent Button B!', 25, 15, 1)
+#         # elif not btnC.value:
+#         #     # Send Button C
+#         #     display.fill(0)
+#         #     button_c_data = bytes("Button C!\r\n","utf-8")
+#         #     rfm9x.send(button_c_data)
+#         #     display.text('Sent Button C!', 25, 15, 1)
 
 
-        # display.show()
-        time.sleep(0.1)
+#         # display.show()
+#         sleep(0.1)
 
-def transmit():
+def transmit(signal):
     # Configure LoRa Radio
     CS = DigitalInOut(board.CE1)
     RESET = DigitalInOut(board.D25)
@@ -163,7 +200,8 @@ def transmit():
     while num_sends <= 2:
         # data = encrypt("This is data")
         # data = bytes("This is data!\r\n","utf-8")
-        data = bytes("ejiinjaewfiaefiafewihefwahieafwhiefwhifaewhaewhfhbwaefhifaeifaewhefhwabfbahwhabfwefbheiawehwbawfbfbhewafhbfeijafeaijnefwfaehaefwhjaefwjaefwbhjiwaefbhijefwijnwehbjewfakejiwafnjifjnkfwjknfeqwnbjkefqwjknefwqbhjkewjnbkewnjkaefwbnjkewnjjknewrajkbhjkjnkefw\r\n","utf-8")
+        data = bytes(f"{signal}\r\n","utf-8")
+        # data = bytes("ejiinjaewfiaefiafewihefwahieafwhiefwhifaewhaewhfhbwaefhifaeifaewhefhwabfbahwhabfwefbheiawehwbawfbfbhewafhbfeijafeaijnefwfaehaefwhjaefwjaefwbhjiwaefbhijefwijnwehbjewfakejiwafnjifjnkfwjknfeqwnbjkefqwjknefwqbhjkewjnbkewnjkaefwbnjkewnjjknewrajkbhjkjnkefw\r\n","utf-8")
         rfm9x.send(data)
         num_sends+=1
 
@@ -184,3 +222,45 @@ def receive():
         packet_text = str(prev_packet, "utf-8")
         print(f"packet = {packet_text}")
         # time.sleep(1)
+
+
+def set_home_button_pressed_callback(channel):
+    #print("Set home button pressed!")
+    transmit("5")
+
+def return_to_home_button_pressed_callback(channel):
+    #print("Return to home button pressed!")
+    transmit("7")
+
+def START_STOP_MOVE_BUTTON_button_pressed_callback(channel):
+    #print("Start/Stop move button pressed!")
+    transmit("9")
+
+def START_STOP_DISPENSING_BUTTON_button_pressed_callback(channel):
+    #print("Start/Stop dispense button pressed!")
+    transmit("11")
+
+def EMERGENCY_STOP_BUTTON_button_pressed_callback(channel):
+    #print("Emergency stop button pressed!")
+    transmit("13")
+
+def dispense_rate_button_pressed_callback(channel):
+    print("Dispense rate button pressed!")
+
+GPIO.add_event_detect(SET_HOME_BUTTON, GPIO.FALLING, callback=set_home_button_pressed_callback, bouncetime=200)
+GPIO.add_event_detect(RETURN_TO_HOME_BUTTON, GPIO.FALLING, callback=return_to_home_button_pressed_callback, bouncetime=200)
+GPIO.add_event_detect(START_STOP_MOVE_BUTTON, GPIO.FALLING, callback=START_STOP_MOVE_BUTTON_button_pressed_callback, bouncetime=200)
+GPIO.add_event_detect(START_STOP_DISPENSING_BUTTON, GPIO.FALLING, callback=START_STOP_DISPENSING_BUTTON_button_pressed_callback, bouncetime=200)
+GPIO.add_event_detect(EMERGENCY_STOP_BUTTON, GPIO.FALLING, callback=EMERGENCY_STOP_BUTTON_button_pressed_callback, bouncetime=200)
+GPIO.add_event_detect(DISPENSE_RATE, GPIO.FALLING, callback=dispense_rate_button_pressed_callback, bouncetime=200)
+
+    # while True:
+    #     GPIO.output(IMMOBILIZED_LIGHT, GPIO.HIGH)
+    #     GPIO.output(OUT_OF_ALGAECIDE_LIGHT, GPIO.LOW)
+    #     sleep(1)
+    #     GPIO.output(IMMOBILIZED_LIGHT, GPIO.LOW)
+    #     GPIO.output(OUT_OF_ALGAECIDE_LIGHT, GPIO.HIGH)
+    #     sleep(1)
+
+    #     print("Analog Value: ", channel.value, "Voltage: ", channel.voltage)
+    #     sleep(0.2)
