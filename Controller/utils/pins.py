49586d8ef21b3893bb844d0fad7e714d4b8b58e5
Regin Potter <potter.70@wright.edu>
2024-03-11 20:06:40 -0400

    Consolidated all pins to one file Added `get` methods to return the pin number Set each pin to its input/output status and set the mode for the board

---
+++ Controller/utils/pins.py
@@ -0,0 +1,44 @@
+import RPI.GPIO as GPIO
+
+start_stop_move = 8
+return_home = 14
+emergency_stop = 15
+set_home = 18
+out_of_algaecide = 20
+immobilized = 21
+change_algaecide = 23
+start_stop_algaecide = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(start_stop_move, GPIO.IN)
+GPIO.setup(return_home, GPIO.IN)
+GPIO.setup(emergency_stop, GPIO.IN)
+GPIO.setup(set_home, GPIO.IN)
+GPIO.setup(out_of_algaecide, GPIO.OUT)
+GPIO.setup(immobilized, GPIO.OUT)
+GPIO.setup(change_algaecide, GPIO.IN)
+GPIO.setup(start_stop_algaecide, GPIO.IN)
+
+def get_stop_start_move():
+    return start_stop_move
+
+def get_return_home():
+    return return_home
+
+def get_emergency_stop():
+    return emergency_stop
+
+def get_set_home():
+    return set_home
+
+def get_out_of_algaecide():
+    return out_of_algaecide
+
+def get_immobilized():
+    return immobilized
+
+def get_change_algaecide():
+    return change_algaecide
+
+def get_start_stop_algaecide():
+    return start_stop_algaecide
---
+++ PEAT/utils/pins.py
@@ -0,0 +1,94 @@
+import RPI.GPIO as GPIO
+
+algae_trig = 2
+algae_echo = 3
+auger_en = 4
+drive_in2 = 6
+left_trig = 12
+turn = 13
+rx_gps = 14
+tx_gps = 15
+left_echo = 16
+auger_in1 = 17
+dispenser_en = 18
+dispenser_in1 = 20
+dispenser_in2 = 21
+drive_in1 = 22
+right_trig = 23
+right_echo = 24
+drive_en = 26
+auger_in2 = 27
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(algae_trig, GPIO.OUT)
+GPIO.setup(algae_echo, GPIO.IN)
+GPIO.setup(auger_en, GPIO.OUT)
+GPIO.setup(drive_in2, GPIO.OUT)
+GPIO.setup(left_trig, GPIO.OUT)
+GPIO.setup(turn, GPIO.OUT)
+GPIO.setup(rx_gps, GPIO.IN)
+GPIO.setup(tx_gps, GPIO.OUT)
+GPIO.setup(left_echo, GPIO.IN)
+GPIO.setup(auger_in1, GPIO.OUT)
+GPIO.setup(dispenser_en, GPIO.OUT)
+GPIO.setup(dispenser_in1, GPIO.OUT)
+GPIO.setup(dispenser_in2, GPIO.OUT)
+GPIO.setup(drive_in1, GPIO.OUT)
+GPIO.setup(right_trig, GPIO.OUT)
+GPIO.setup(right_echo, GPIO.IN)
+GPIO.setup(drive_en, GPIO.OUT)
+GPIO.setup(auger_in2, GPIO.OUT)
+
+def get_algae_trig():
+    return algae_trig
+
+def get_algae_echo():
+    return algae_echo
+
+def get_auger_en():
+    return auger_en
+
+def get_drive_in2():
+    return drive_in2
+
+def get_left_trig():
+    return left_trig
+
+def get_turn():
+    return turn
+
+def get_rx_gps():
+    return rx_gps
+
+def get_tx_gps():
+    return tx_gps
+
+def get_left_echo():
+    return left_echo
+
+def get_auger_in1():
+    return auger_in1
+
+def get_dispenser_en():
+    return dispenser_en
+
+def get_dispenser_in1():
+    return dispenser_in1
+
+def get_dispenser_in2():
+    return dispenser_in2
+
+def get_drive_in1():
+    return drive_in1
+
+def get_right_trig():
+    return right_trig
+
+def get_right_echo():
+    return right_echo
+
+def get_drive_en():
+    return drive_en
+
+def get_auger_in2():
+    return auger_in2
