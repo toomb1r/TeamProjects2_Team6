e13f69a404e95e7bf3c57b997cb544b0ed3bd550
Regin Potter <potter.70@wright.edu>
2024-03-19 09:45:35 -0400

    Merge dev into Fin_Move

--- PEAT/main.py
+++ PEAT/main.py
@@ -42,34 +42,14 @@ def main():
 
     # enc_msg = encrypt("this is encrypted")
     # dec_msg = decrypt(enc_msg)
-
+    # This handles CTRL+C stuff and signal.pause pauses the main method (think while(true) loop)
+    # signal.pause()
+    signal.signal(signal.SIGINT, signal_handler)
     # receive()
     while True:
         if left_dist() < 30:
             transmit("1")
         sleep(5)
-    #sleep(5)
-
-    # transmit_and_receive()
-
-    # This is how to make an interrupt, this is commented out because idk how to get
-    # the string from what is being called in from the controller...
-    # GPIO.add_event_detect(9, GPIO.FALLING, callback=decrypt())
-
-    # This is to start the servo motor in the center of the 180 degrees
-    # To allow -90 and 90 degrees of motion
-    turning(90)
-
-    move()
-    rorl = True
-
-    # This handles CTRL+C stuff and signal.pause pauses the main method (think while(true) loop)
-    signal.signal(signal.SIGINT, signal_handler)
-    # signal.pause()
-
-    while(True):
-        edgeOfPond(rorl)
-        move()
 
 if __name__ == "__main__":
     main()
