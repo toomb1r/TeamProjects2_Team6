e051e833882808a5b8daaa912ae858b8babfa711
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2024-03-26 10:21:41 -0400

    240 chars

--- Controller/utils/communications.py
+++ Controller/utils/communications.py
@@ -110,7 +110,7 @@ def transmit(signal):
 
     data = encrypt(signal)
     # https://www.geeksforgeeks.org/python-split-string-in-groups-of-n-consecutive-characters/
-    data_list = [(data[i:i+250]) for i in range(0, len(data), 250)]
+    data_list = [(data[i:i+240]) for i in range(0, len(data), 240)]
     # data_list[-1] = data_list[-1] + "\n"
     data_list.append(b"\n")
     print(data_list)
