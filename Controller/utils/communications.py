d44452ad9ad8fc6e3a1dbd4a4fb4d912cc338a56
Anmol Saini <anmolsaini@Anmols-MacBook-Pro.local>
2024-03-27 18:37:14 -0400

    5 sends

--- Controller/utils/communications.py
+++ Controller/utils/communications.py
@@ -115,7 +115,7 @@ def transmit(signal):
     data_list.append(b"\n")
     for seg in data_list:
         num_sends = 0
-        while num_sends <= 2:
+        while num_sends <= 5:
             rfm9x.send(seg)
             num_sends+=1
 
