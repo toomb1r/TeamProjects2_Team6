44d39a75d2a2985be022c3f5373bbde524169e2e
Regin Potter <potter.70@wright.edu>
2024-04-01 18:25:07 -0400

    Type casting

--- PEAT/utils/gps_test.py
+++ PEAT/utils/gps_test.py
@@ -48,8 +48,8 @@ def convert_to_degrees(raw_value):
 
 def convert_to_meters(lat1, lon1, lat2, lon2):
     R = 6378.137
-    dLat = lat2 * math.pi / 180 - lat1 * math.pi / 180
-    dLon = lon2 * math.pi / 180 - lon1 * math.pi / 180
+    dLat = int(lat2) * math.pi / 180 - int(lat1) * math.pi / 180
+    dLon = int(lon2) * math.pi / 180 - int(lon1) * math.pi / 180
     a = math.sin(dLat/2) * math.sin(dLat/2) + math.cos(lat1 * math.pi / 180) * math.cos(lat2 * math.pi / 180) * math.sin(dLon/2) * math.sin(dLon/2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
     d = R * c
