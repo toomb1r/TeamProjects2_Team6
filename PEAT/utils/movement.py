e9c9737156ff42a2191f4625561a258a52052334
Regin Potter <potter.70@wright.edu>
2024-04-03 20:18:39 -0400

    data?

--- PEAT/utils/movement.py
+++ PEAT/utils/movement.py
@@ -240,7 +240,7 @@ def edgeOfPond():
             data = file.read().strip()
             file.close()
             print(f"Data: {data}\n\n")
-        print("not stopped")
+        print(f"not stopped {data}")
         left = left_dist()
         if left <= 25 and left > 5.5:
             if data == "stop":
