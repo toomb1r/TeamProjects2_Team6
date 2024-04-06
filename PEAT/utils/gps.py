06e82a535ac9b888caac2fc3c35ffa60d11a4b92
Regin Potter <potter.70@wright.edu>
2024-04-06 17:01:43 -0400

    prints

--- PEAT/utils/gps.py
+++ PEAT/utils/gps.py
@@ -34,7 +34,7 @@ def GPS_Info():
 
     if nmea_latitude == "" or nmea_longitude == "":
         return
-    
+
     lat = float(nmea_latitude)                  #convert string into float for calculation
     longi = float(nmea_longitude)               #convertr string into float for calculation
 
@@ -110,6 +110,7 @@ def readHome():
     """
 
     global home
+    print("reading home")
     with open("home.txt","r") as file:
         home = file.readlines()
 
