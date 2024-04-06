0dd01991579f81ebd972a87d8a4063cecab06c63
Regin Potter <potter.70@wright.edu>
2024-04-05 22:38:27 -0400

    removing testing print statements

--- PEAT/main.py
+++ PEAT/main.py
@@ -69,18 +69,14 @@ def receive_state():
     zero = False
     sixty = False
     while True:
-        print("while loop")
         received_sig = ""
         if time() - start_receive > 0 and not zero:
-            print("finding distance")
             # find_distance()
             zero = True
         elif time() - start_receive > 60 and not sixty:
             # find_distance()
             sixty = True
-        print("before try")
         try:
-            print("trying to receive")
             received_sig = receive(60.0).strip()
         except:
             print("Error: Receive failed\n")
